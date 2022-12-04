01fc6268ac9deca3659284004b57874e744f02a0
raniazayed <raniamagdy95@gmail.com>
2022-12-04 20:07:41 +0200

    remove commented code

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
 const App = () => {
-    // const text = marketingMount(document.getElementById('root'))
-    return <>
-        <h1>Container </h1>;
-        <MarketingApp />
+  return (
+    <>
+      <h1>Container </h1>;
+      <MarketingApp />
     </>
-}
+  );
+};
 export default App;
